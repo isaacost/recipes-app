5c7cb9efd3fd7e53709510c30853a7581a7edc77
Rafael <bechstedt@hotmail.com>
2022-10-24 18:58:57 -0300

    Rotas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route } from 'react-router-dom';
 import MealProvider from './contexts/MealProvider';
 import CocktailProvider from './contexts/CocktailProvider';
-import Header from './components/Header';
+import Login from './pages/Login';
+import Meals from './pages/Meals';
+import Drinks from './pages/Drinks';
+import MealsIdReceita from './pages/MealsIdReceita';
+import DrinksIdReceita from './pages/DrinksIdReceita';
+import MealsInProgress from './pages/MealsInProgress';
+import DrinksInProgress from './pages/DrinksInProgress';
+import Profile from './pages/Profile';
+import DoneRecipes from './pages/DoneRecipes';
+import FavoriteRecipes from './pages/FavoriteRecipes';
 
 function App() {
   return (
     <MealProvider>
       <CocktailProvider>
-       <Header />
-        <div className="meals">
-          <span className="logo">TRYBE</span>
-          <object
-            className="rocksGlass"
-            type="image/svg+xml"
-            data={ rockGlass }
-          >
-            Glass
-          </object>
-        </div>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/meals" component={ Meals } />
+        <Route exact path="/drinks" component={ Drinks } />
+        <Route exact path="/meals/:id-da-receita" component={ MealsIdReceita } />
+        <Route exact path="/drinks/:id-da-receita" component={ DrinksIdReceita } />
+        <Route
+          exact
+          path="/meals/:id-da-receita/in-progress"
+          component={ MealsInProgress }
+        />
+        <Route
+          exact
+          path="/drinks/:id-da-receita/in-progress"
+          component={ DrinksInProgress }
+        />
+        <Route exact path="/profile" component={ Profile } />
+        <Route exact path="/done-recipes" component={ DoneRecipes } />
+        <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
       </CocktailProvider>
     </MealProvider>
   );
---
+++ src/pages/DrinksInProgress.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function DrinksInProgress() {
+  return (
+    <div>DrinksInProgress</div>
+  );
+}
