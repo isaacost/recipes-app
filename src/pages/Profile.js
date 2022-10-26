e1f0d696ae26cdd154fd3abba2a919ddff45c68d
Isa Costa <isa-hcosta@hotmail.com>
2022-10-25 21:44:23 -0300

    componente Profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import MealContext from '../contexts/MealContext';
 
 export default function Profile() {
+  const { handleDoneRecipes,
+    handleFavoriteRecipes,
+    handleLogout } = useContext(MealContext);
+  const email = JSON.parse(localStorage.getItem('user'));
+  // console.log(Object.values(email));
   return (
     <div>
       <Header title="Profile" />
-      Profile
+      <p data-testid="profile-email">
+        { Object.values(email) }
+      </p>
+
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ handleDoneRecipes }
+      >
+        Done Recipes
+      </button>
+
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ handleFavoriteRecipes }
+      >
+        Favorite Recipes
+      </button>
+
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ handleLogout }
+      >
+        Logout
+      </button>
       <Footer />
     </div>
   );
