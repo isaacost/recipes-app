2fd09c2573b23ec0f1367e358550aa799c9f6fb6
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2022-10-28 15:59:44 -0300

    requisito 36

--- src/components/CheckboxList.js
+++ src/components/CheckboxList.js
@@ -4,6 +4,14 @@ import { RecipesContext } from '../contexts/RecipesContext';
 export default function CheckboxList() {
   const { ingredientsList, measureList } = useContext(RecipesContext);
 
+  const handleChange = ({ target }) => {
+    if (target.checked) {
+      target.parentNode.style.textDecoration = 'line-through solid rgb(0, 0, 0)';
+    } else {
+      target.parentNode.style.textDecoration = '';
+    }
+  };
+
   return (
     <ul>
       {ingredientsList.map((ingredient, index) => (
@@ -16,6 +24,7 @@ export default function CheckboxList() {
               type="checkbox"
               id={ ingredient }
               value={ ingredient }
+              onChange={ handleChange }
             />
             {`${measureList[index]} ${ingredient}`}
           </label>
