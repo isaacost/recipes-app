a0b5289f796922fd16fe88f1830e0a28362afb2e
Renan Casarin Munhoes <munhoesrc@gmail.com>
2022-10-31 11:35:17 -0300

    requisito 44

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import DoneRecipesCard from '../components/CardDoneRecipes';
 
 export default function DoneRecipes() {
   return (
     <div>
       <Header title="Done Recipes" />
-      DoneRecipes
+      <DoneRecipesCard />
     </div>
   );
 }
