9d0af1940516911f19a41ff385e90a9f872befaf
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2022-11-01 17:30:11 -0300

    testes checkbox

--- src/tests/RecipeInProgress.test.js
+++ src/tests/RecipeInProgress.test.js
@@ -39,6 +39,75 @@ const FAVORITE_DRINK_MOCK = {
 const IN_PROGRESS_MEAL_ROUTE = '/meals/52771/in-progress';
 const IN_PROGRESS_DRINK_ROUTE = '/drinks/178319/in-progress';
 
+const ONE_DRINK_MOCK = {
+  drinks: [
+    {
+      idDrink: '15997',
+      strDrink: 'GG',
+      strDrinkAlternate: null,
+      strDrinkES: null,
+      strDrinkDE: null,
+      strDrinkFR: null,
+      'strDrinkZH-HANS': null,
+      'strDrinkZH-HANT': null,
+      strTags: null,
+      strVideo: null,
+      strCategory: 'Ordinary Drink',
+      strIBA: null,
+      strAlcoholic: 'Optional alcohol',
+      strGlass: 'Collins Glass',
+      strInstructions: 'Pour the Galliano liqueur over ice. Fill the remainder of the glass with ginger ale and thats all there is to it. You now have a your very own GG.',
+      strInstructionsES: null,
+      strInstructionsDE: 'Den Galliano-Likör über Eis gießen. Füllen Sie den Rest des Glases mit Ginger Ale und das ist alles, was dazu gehört. Du hast jetzt ein eigenes GG.',
+      strInstructionsFR: null,
+      'strInstructionsZH-HANS': null,
+      'strInstructionsZH-HANT': null,
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/vyxwut1468875960.jpg',
+      strIngredient1: 'Galliano',
+      strIngredient2: 'Ginger ale',
+      strIngredient3: 'Ice',
+      strIngredient4: null,
+      strIngredient5: null,
+      strIngredient6: null,
+      strIngredient7: null,
+      strIngredient8: null,
+      strIngredient9: null,
+      strIngredient10: null,
+      strIngredient11: null,
+      strIngredient12: null,
+      strIngredient13: null,
+      strIngredient14: null,
+      strIngredient15: null,
+      strMeasure1: '2 1/2 shots ',
+      strMeasure2: null,
+      strMeasure3: null,
+      strMeasure4: null,
+      strMeasure5: null,
+      strMeasure6: null,
+      strMeasure7: null,
+      strMeasure8: null,
+      strMeasure9: null,
+      strMeasure10: null,
+      strMeasure11: null,
+      strMeasure12: null,
+      strMeasure13: null,
+      strMeasure14: null,
+      strMeasure15: null,
+      strCreativeCommonsConfirmed: 'No',
+      dateModified: '2016-07-18 22:06:00',
+    },
+  ],
+};
+
+const IN_PROGRESS_LOCAL_MOCK = {
+  drinks: {
+    15997: [
+      'Galliano',
+    ],
+  },
+  meals: {},
+};
+
 describe('Testando componente RecipeInProgress', () => {
   it('Testa botão de compartilhar', async () => {
     api.getRecipes.mockResolvedValue(drinks);
@@ -194,5 +263,24 @@ describe('Testando componente RecipeInProgress', () => {
 
     expect(window.location.pathname).toBe('/done-recipes');
     expect(JSON.parse(localStorage.getItem('doneRecipes')).length).toBe(2);
+  });
+
+  it('Testa checkbox quando inicializa', async () => {
+    api.getRecipes.mockResolvedValueOnce(meals);
+    api.getRecipeDetails.mockResolvedValueOnce(ONE_DRINK_MOCK);
+
+    localStorage.setItem('inProgressRecipes', JSON.stringify(IN_PROGRESS_LOCAL_MOCK));
+    await act(async () => { renderWithRouter(<App />, '/drinks/15997/in-progress'); });
+
+    const checkboxes = screen.getAllByRole('checkbox');
+    expect(checkboxes[0].checked).toBe(true);
+    expect(checkboxes[0].parentElement.textContent).toBe('2 1/2 shots  Galliano');
+    expect(checkboxes[1].checked).toBe(false);
+
+    userEvent.click(checkboxes[1]);
+    expect(checkboxes[1].parentElement.style.textDecoration).toBe('line-through solid rgb(0, 0, 0)');
+    userEvent.click(checkboxes[1]);
+    expect(checkboxes[1].parentElement.style.textDecoration).toBe('');
+    expect(checkboxes[1].parentElement.textContent).toBe(' Ginger ale');
   });
 });
